85dd0acfc8100e0a5e55665f09bb18cf3a943f34
Adam Chester <adamchester@gmail.com>
2013-10-09 01:40:02 +0930

    Added visitor code

---
+++ Src/Albedo/IHierarchicalReflectionElement.cs
@@ -0,0 +1,13 @@
+﻿namespace Ploeh.Albedo
+{
+    public interface IHierarchicalReflectionElement
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
+        IHierarchicalReflectionVisitor<T> Accept<T>(IHierarchicalReflectionVisitor<T> visitor);        
+    }
+}
