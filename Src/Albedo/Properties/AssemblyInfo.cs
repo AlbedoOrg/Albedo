b78a7a27f2665f198596bf5466f531dd8380d21e
Adam Chester <adamchester@gmail.com>
2013-10-09 22:11:44 +0930

    Added missing CLSCompliant attribute to Albedo.dll

--- Src/Albedo/Properties/AssemblyInfo.cs
+++ Src/Albedo/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -18,6 +19,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7b2ea5eb-399c-49b6-92cc-3746a1794740")]
