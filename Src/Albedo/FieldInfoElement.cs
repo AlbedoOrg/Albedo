7a514eefffe29056e6a599a50e68111008c4ff65
Adam Chester <adamchester@gmail.com>
2013-10-09 21:05:57 +0930

    Added visitor code comments

--- Src/Albedo/FieldInfoElement.cs
+++ Src/Albedo/FieldInfoElement.cs
@@ -9,20 +9,52 @@ namespace Ploeh.Albedo
     /// </summary>
     public class FieldInfoElement : IReflectionElement
     {
+        /// <summary>
+        /// Gets the <see cref="System.Reflection.FieldInfo"/> instance this element points to.
+        /// </summary>
         public FieldInfo FieldInfo { get; private set; }
 
+        /// <summary>
+        /// Constructs a new instance of the <see cref="FieldInfoElement"/> which represents
+        /// the specified <see cref="System.Reflection.FieldInfo"/>.
+        /// </summary>
+        /// <param name="fieldInfo">The <see cref="System.Reflection.FieldInfo"/> this 
+        /// element represents.</param>
         public FieldInfoElement(FieldInfo fieldInfo)
         {
             if (fieldInfo == null) throw new ArgumentNullException("fieldInfo");
             FieldInfo = fieldInfo;
         }
 
+        /// <summary>
+        /// Accepts the provided <see cref="IReflectionVisitor{T}"/>, by calling the
+        /// appropriate strongly-typed <see cref="IReflectionVisitor{T}.Visit(FieldInfoElement)"/>
+        /// method on the visitor.
+        /// </summary>
+        /// <typeparam name="T">The type of observation or result which the
+        /// <see cref="IReflectionVisitor{T}"/> instance produces when visiting nodes.</typeparam>
+        /// <param name="visitor">The <see cref="IReflectionVisitor{T}"/> instance.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public IReflectionVisitor<T> Accept<T>(IReflectionVisitor<T> visitor)
         {
             if (visitor == null) throw new ArgumentNullException("visitor");
             return visitor.Visit(this);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current
+        /// <see cref="T:System.Object"/>. 
+        /// </summary>
+        /// <returns>
+        /// true if the specified <see cref="T:System.Object"/> is equal to the current 
+        /// <see cref="T:System.Object"/>; otherwise, false. 
+        /// </returns>
+        /// <param name="obj">The <see cref="T:System.Object"/> to compare with the current
+        /// <see cref="T:System.Object"/>.
+        /// </param>
+        /// <exception cref="T:System.NullReferenceException">The <paramref name="obj"/>
+        /// parameter is null.</exception>
         public override bool Equals(object obj)
         {
             if (object.ReferenceEquals(null, obj))
@@ -43,6 +75,12 @@ namespace Ploeh.Albedo
             return this.FieldInfo.Equals(((FieldInfoElement)obj).FieldInfo);
         }
 
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary> 
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>. 
+        /// </returns>
         public override int GetHashCode()
         {
             return this.FieldInfo.GetHashCode();
--- Src/Albedo/ReflectionVisitor.cs
+++ Src/Albedo/ReflectionVisitor.cs
@@ -2,43 +2,96 @@
 {
     /// <summary>
     /// An implementation of the <see cref="IReflectionVisitor{T}"/> which does
-    /// not visit any type of element, allowing easier implementation when only
-    /// certain element types need to be visited.
+    /// not visit any type of element. This allows easier implementation of the
+    /// <see cref="IReflectionVisitor{T}"/> interface, when only certain element
+    /// types need to be visited.
     /// </summary>
     public abstract class ReflectionVisitor<T> : IReflectionVisitor<T>
     {
+        /// <summary>
+        /// Gets the observations or values produced by this visitor instance.
+        /// </summary>
         public abstract T Value { get; }
 
+        /// <summary>
+        /// Allows an <see cref="AssemblyElement"/> to be 'visited'. Override this method to allow
+        /// the derived visitor to observe the element.
+        /// </summary>
+        /// <param name="assemblyElement">The <see cref="AssemblyElement"/> being visited.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public virtual IReflectionVisitor<T> Visit(AssemblyElement assemblyElement)
         {
             return this;
         }
 
+        /// <summary>
+        /// Allows an <see cref="TypeElement"/> to be 'visited'. Override this method to allow
+        /// the derived visitor to observe the element.
+        /// </summary>
+        /// <param name="typeElement">The <see cref="TypeElement"/> being visited.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public virtual IReflectionVisitor<T> Visit(TypeElement typeElement)
         {
             return this;
         }
 
+        /// <summary>
+        /// Allows an <see cref="ParameterInfoElement"/> to be 'visited'. Override this method to allow
+        /// the derived visitor to observe the element.
+        /// </summary>
+        /// <param name="parameterInfoElement">The <see cref="ParameterInfoElement"/> being visited.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public virtual IReflectionVisitor<T> Visit(ParameterInfoElement parameterInfoElement)
         {
             return this;
         }
 
+        /// <summary>
+        /// Allows an <see cref="PropertyInfoElement"/> to be 'visited'. Override this method to allow
+        /// the derived visitor to observe the element.
+        /// </summary>
+        /// <param name="propertyInfoElement">The <see cref="PropertyInfoElement"/> being visited.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public virtual IReflectionVisitor<T> Visit(PropertyInfoElement propertyInfoElement)
         {
             return this;
         }
 
+        /// <summary>
+        /// Allows an <see cref="FieldInfoElement"/> to be 'visited'. Override this method to allow
+        /// the derived visitor to observe the element.
+        /// </summary>
+        /// <param name="fieldInfoElement">The <see cref="FieldInfoElement"/> being visited.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public virtual IReflectionVisitor<T> Visit(FieldInfoElement fieldInfoElement)
         {
             return this;
         }
 
+        /// <summary>
+        /// Allows an <see cref="ConstructorInfoElement"/> to be 'visited'. Override this method to allow
+        /// the derived visitor to observe the element.
+        /// </summary>
+        /// <param name="constructorInfoElement">The <see cref="ConstructorInfoElement"/> being visited.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public virtual IReflectionVisitor<T> Visit(ConstructorInfoElement constructorInfoElement)
         {
             return this;
         }
 
+        /// <summary>
+        /// Allows an <see cref="MethodInfoElement"/> to be 'visited'. Override this method to allow
+        /// the derived visitor to observe the element.
+        /// </summary>
+        /// <param name="methodInfoElement">The <see cref="MethodInfoElement"/> being visited.</param>
+        /// <returns>A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.</returns>
         public virtual IReflectionVisitor<T> Visit(MethodInfoElement methodInfoElement)
         {
             return this;
