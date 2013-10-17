b9b37488e6d38c07f1c1e93e4364378a3b89c815
Mark Seemann <mark@ploeh.dk>
2013-10-17 23:52:50 +0200

    Added a couple of Scenario tests.
    These are intended to be the introductory tests for the Albedo library,
    i.e. the first tests a new user should review. They are optimized to be
    readable even without an IDE, and thus minimized the use of the 'var'
    keyword, etc.

---
+++ Src/Albedo.UnitTests/Scenario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class Scenario
+    {
+        [Fact]
+        public void SelectParameterLessMethodUsingInstanceSyntax()
+        {
+            MethodInfo mi = new Methods<Version>().Select(v => v.ToString());
+            Assert.Equal("ToString", mi.Name);
+        }
+
+        [Fact]
+        public void SelectParameterLessMethodUsingLinqSyntax()
+        {
+            MethodInfo mi = from v in new Methods<Version>()
+                            select v.ToString();
+            Assert.Equal("ToString", mi.Name);
+        }
+    }
+}
