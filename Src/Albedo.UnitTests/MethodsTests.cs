f4bca046f84847b9b0030e0a92cef9c6b2fc9729
Mark Seemann <mark@ploeh.dk>
2013-10-17 23:18:33 +0200

    Added Method<T>, and a Select method.

---
+++ Src/Albedo.UnitTests/MethodsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Albedo;
+using System.Reflection;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class MethodsTests
+    {
+        [Fact]
+        public void SelectParameterLessReturnsCorrectMethod()
+        {
+            var sut = new Methods<ClassWithMethods>();
+            
+            MethodInfo actual = sut.Select(x => x.OmitParameters());
+
+            var expected = typeof(ClassWithMethods).GetMethod("OmitParameters");
+            Assert.Equal(expected, actual);
+        }
+
+        private class ClassWithMethods
+        {
+            public void OmitParameters()
+            {
+            }
+        }
+    }
+}
---
+++ Src/Albedo/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public class Methods<T>
+    {
+        public MethodInfo Select(
+            Expression<Action<T>> methodSelector)
+        {
+            return ((MethodCallExpression)methodSelector.Body).Method;
+        }
+    }
+}
