6a0ae2498948c27006359f716e495b5a5e90647f
Mark Seemann <mark@ploeh.dk>
2013-10-18 20:58:57 +0200

    Added Properties<T>,
    although its single Select method is still quite unsafe.

---
+++ Src/Albedo.UnitTests/PropertiesTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Albedo;
+using System.Reflection;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class PropertiesTests
+    {
+        [Fact]
+        public void SelectPropertyReturnsCorrectProperty()
+        {
+            var sut = new Properties<ClassWithProperties>();
+
+            PropertyInfo actual = sut.Select(x => x.ReadOnlyText);
+
+            var expected = typeof(ClassWithProperties).GetProperty("ReadOnlyText");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void QueryPropertyUsingLinqSyntax()
+        {
+            var sut = new Properties<ClassWithProperties>();
+
+            var actual = from x in sut select x.ReadOnlyText;
+
+            var expected = typeof(ClassWithProperties).GetProperty("ReadOnlyText");
+            Assert.Equal(expected, actual);
+        }
+
+        private class ClassWithProperties
+        {
+            public string ReadOnlyText { get; private set; }
+        }
+    }
+}
---
+++ Src/Albedo/Properties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public class Properties<T>
+    {
+        public PropertyInfo Select<TProperty>(Expression<Func<T, TProperty>> propertySelector)
+        {
+            return (PropertyInfo)((MemberExpression)propertySelector.Body).Member;
+        }
+    }
+}
