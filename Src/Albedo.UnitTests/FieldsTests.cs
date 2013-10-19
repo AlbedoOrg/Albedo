4d8b1a301971e2653e1845532e059bf359c5da96
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-10-19 19:07:41 +0300

    Introduced Fields<T> class.

---
+++ Src/Albedo.UnitTests/FieldsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Albedo;
+using System.Reflection;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class FieldsTests
+    {
+        [Fact]
+        public void SelectFieldReturnsCorrectField()
+        {
+            var sut = new Fields<ClassWithFields>();
+
+            FieldInfo actual = sut.Select(x => x.ReadOnlyText);
+
+            var expected = typeof(ClassWithFields).GetField("ReadOnlyText");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void QueryFieldUsingLinqSyntaxReturnsCorrectField()
+        {
+            var sut = new Fields<ClassWithFields>();
+
+            FieldInfo actual = from x in sut select x.ReadOnlyText;
+
+            var expected = typeof(ClassWithFields).GetField("ReadOnlyText");
+            Assert.Equal(expected, actual);
+        }
+
+        private class ClassWithFields
+        {
+            public readonly string ReadOnlyText = string.Empty;
+        }
+    }
+}
---
+++ Src/Albedo/Fields.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public class Fields<T>
+    {
+        public FieldInfo Select<TField>(Expression<Func<T, TField>> fieldSelector)
+        {
+            return (FieldInfo)((MemberExpression)fieldSelector.Body).Member;
+        }
+    }
+}
