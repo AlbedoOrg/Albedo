15fb82849785affe5448c2703678744f45879cc7
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-10-30 08:00:09 +0200

    Made CompositeReflectionElement class an Iterator.

--- Src/Albedo.UnitTests/CompositeReflectionElementTests.cs
+++ Src/Albedo.UnitTests/CompositeReflectionElementTests.cs
@@ -1,3 +1,4 @@
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,28 @@ namespace Ploeh.Albedo.UnitTests
         {
             var sut = new CompositeReflectionElement();
             Assert.IsAssignableFrom<IReflectionElement>(sut);
+        }
+
+        [Fact]
+        public void SutIsSequence()
+        {
+            var sut = new CompositeReflectionElement();
+            Assert.IsAssignableFrom<IEnumerable<IReflectionElement>>(sut);
+        }
+
+        [Fact]
+        public void SutYieldsInjectedElements()
+        {
+            var expected = new[]
+            {
+                new Mock<IReflectionElement>().Object,
+                new Mock<IReflectionElement>().Object,
+                new Mock<IReflectionElement>().Object
+            };
+            var sut = new CompositeReflectionElement(expected);
+            Assert.True(expected.SequenceEqual(sut));
+            Assert.True(
+                expected.Cast<object>().SequenceEqual(sut.OfType<object>()));
         }
     }
 }
--- Src/Albedo/CompositeReflectionElement.cs
+++ Src/Albedo/CompositeReflectionElement.cs
@@ -5,11 +5,28 @@ using System.Text;
 
 namespace Ploeh.Albedo
 {
-    public class CompositeReflectionElement : IReflectionElement
+    public class CompositeReflectionElement : IReflectionElement, IEnumerable<IReflectionElement>
     {
+        private readonly IEnumerable<IReflectionElement> elements;
+
+        public CompositeReflectionElement(params IReflectionElement[] elements)
+        {
+            this.elements = elements;
+        }
+
         public IReflectionVisitor<T> Accept<T>(IReflectionVisitor<T> visitor)
         {
             throw new NotImplementedException();
+        }
+
+        public IEnumerator<IReflectionElement> GetEnumerator()
+        {
+            return this.elements.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 }
