2ae112c3cb496905141053d95564a50af3f35b83
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-10-30 08:09:05 +0200

    Added XML documentation to CompositeReflectionElement class.

--- Src/Albedo/CompositeReflectionElement.cs
+++ Src/Albedo/CompositeReflectionElement.cs
@@ -5,20 +5,48 @@ using System.Text;
 
 namespace Ploeh.Albedo
 {
+    /// <summary>
+    /// An implementation of a polymorphic <see cref="IReflectionElement"/> 
+    /// that composes other <see cref="IReflectionElement"/> instances which
+    /// can be visited by an <see cref="IReflectionVisitor{T}"/> instance.
+    /// </summary>
     public class CompositeReflectionElement : IReflectionElement, IEnumerable<IReflectionElement>
     {
         private readonly IEnumerable<IReflectionElement> elements;
 
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="CompositeReflectionElement"/> class.
+        /// </summary>
+        /// <param name="elements">
+        /// The <see cref="IReflectionElement"/> instances to compose.</param>
         public CompositeReflectionElement(params IReflectionElement[] elements)
         {
             this.elements = elements;
         }
 
+        /// <summary>
+        /// Accepts the <see cref="IReflectionVisitor{T}"/> visitor.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of observation(s) of the vistor.
+        /// </typeparam>
+        /// <param name="visitor">The visitor to accept.</param>
+        /// <returns></returns>
         public IReflectionVisitor<T> Accept<T>(IReflectionVisitor<T> visitor)
         {
             return this.elements.Aggregate(visitor, (v, e) => e.Accept(v));
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the supplied 
+        /// <see cref="IReflectionElement"/> instances.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{IReflectionElement}" /> that can be used to
+        /// iterate through the supplied <see cref="IReflectionElement"/> 
+        /// instances.
+        /// </returns>
         public IEnumerator<IReflectionElement> GetEnumerator()
         {
             return this.elements.GetEnumerator();
