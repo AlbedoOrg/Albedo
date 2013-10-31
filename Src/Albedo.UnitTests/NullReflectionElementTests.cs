297094587f6cb2b6b7710cee9638e17b79f68199
Mark Seemann <mark@ploeh.dk>
2013-10-31 12:55:06 +0100

    Introduced NullReflectionElement class.

---
+++ Src/Albedo.UnitTests/NullReflectionElementTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Albedo;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class NullReflectionElementTests
+    {
+        [Fact]
+        public void SutIsReflectionElement()
+        {
+            var sut = new NullReflectionElement();
+            Assert.IsAssignableFrom<IReflectionElement>(sut);
+        }
+    }
+}
---
+++ Src/Albedo/NullReflectionElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public class NullReflectionElement : IReflectionElement
+    {
+        public IReflectionVisitor<T> Accept<T>(IReflectionVisitor<T> visitor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
