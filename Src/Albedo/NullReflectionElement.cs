58261938fb2f4e804dbaa786ed71d019f9159086
Mark Seemann <mark@ploeh.dk>
2013-10-31 13:02:10 +0100

    Documented NullReflectionElement.

--- Src/Albedo/NullReflectionElement.cs
+++ Src/Albedo/NullReflectionElement.cs
@@ -5,8 +5,28 @@ using System.Text;
 
 namespace Ploeh.Albedo
 {
+    /// <summary>
+    /// An implementation of IReflectionElement that does nothing.
+    /// </summary>
     public class NullReflectionElement : IReflectionElement
     {
+        /// <summary>
+        /// Accepts the <see cref="IReflectionVisitor{T}" /> as per the
+        /// Visitor pattern http://en.wikipedia.org/wiki/Visitor_pattern.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of observation(s) the visitor might collect.
+        /// </typeparam>
+        /// <param name="visitor">The visitor to accept.</param>
+        /// <returns><paramref name="visitor" /></returns>
+        /// <remarks>
+        /// <para>
+        /// While <strong>NullReflection</strong> partakes in a Visitor
+        /// hierarchy, this particular implementation follows the Null Object
+        /// pattern, by doing nothing. The way it does nothing is by returning
+        /// <paramref name="visitor" />.
+        /// </para>
+        /// </remarks>
         public IReflectionVisitor<T> Accept<T>(IReflectionVisitor<T> visitor)
         {
             return visitor;
