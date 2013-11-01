1378fcde4bdcd1e4bd2a116e49d1bcb8e96c1639
Mark Seemann <mark@ploeh.dk>
2013-11-01 15:17:15 +0100

    Introduced IReflectionElementMaterializer
    and AssemblyElementMaterializer, although the implementation is still
    incorrect.

---
+++ Src/Albedo/IReflectionElementMaterializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public interface IReflectionElementMaterializer<T>
+    {
+        IEnumerable<IReflectionElement> Materialize(IEnumerable<T> source);
+    }
+}
