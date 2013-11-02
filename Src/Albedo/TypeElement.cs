8a5e9314d4281679885a6f57a803b49d452539fb
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-11-02 21:31:33 +0200

    Added XML documentation.

--- Src/Albedo/TypeElement.cs
+++ Src/Albedo/TypeElement.cs
@@ -43,6 +43,22 @@ namespace Ploeh.Albedo
             return visitor.Visit(this);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="Object" />, is equal to
+        /// this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="Object" /> to compare with this
+        /// instance.</param>
+        /// <returns>
+        /// <see langword="true" /> if the specified <see cref="Object" /> is
+        /// equal to this instance; otherwise, <see langword="false" />.
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// Two instances of <see cref="TypeElement" /> are considered to
+        /// be equal if their <see cref="Type" /> values are equal.
+        /// </para>
+        /// </remarks>
         public override bool Equals(object obj)
         {
             var other = obj as TypeElement;
@@ -52,6 +68,13 @@ namespace Ploeh.Albedo
             return object.Equals(this.Type, other.Type);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing
+        /// algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode()
         {
             return this.Type.GetHashCode();
