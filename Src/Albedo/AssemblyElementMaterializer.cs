628e1b2fc0ec56097091a0c4ce9dd0e151859261
Mark Seemann <mark@ploeh.dk>
2013-11-02 22:17:08 +0100

    Documented AssemblyElementMaterializer.

--- Src/Albedo/AssemblyElementMaterializer.cs
+++ Src/Albedo/AssemblyElementMaterializer.cs
@@ -6,8 +6,36 @@ using System.Text;
 
 namespace Ploeh.Albedo
 {
+    /// <summary>
+    /// Creates <see cref="AssemblyElement" /> instances from a sequence of
+    /// source objects.
+    /// </summary>
+    /// <typeparam name="T">The type of source objects.</typeparam>
+    /// <seealso cref="Materialize(IEnumerable{T})" />
     public class AssemblyElementMaterializer<T> : IReflectionElementMaterializer<T>
     {
+        /// <summary>
+        /// Creates <see cref="AssemblyElement" /> instances from a sequence of
+        /// source objects.
+        /// </summary>
+        /// <param name="source">The source objects.</param>
+        /// <returns>
+        /// A sequence of <see cref="AssemblyElement" /> instances created from
+        /// a <paramref name="source" />.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="source" /> is null.
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// This method creates <see cref="AssemblyElement" /> instances from
+        /// all matching elements in <paramref name="source" />. An element is
+        /// matching if it's an <see cref="Assembly" /> instance, in which case
+        /// a corresponding <strong>AssemblyElement</strong> is created and
+        /// returned.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="IReflectionElementMaterializer{T}" />
         public IEnumerable<IReflectionElement> Materialize(IEnumerable<T> source)
         {
             if (source == null)
