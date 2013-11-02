f97a3869ddfc56e2364e5e5c180c4fbea40a6220
Mark Seemann <mark@ploeh.dk>
2013-11-02 22:29:14 +0100

    Introduced TypeElementMaterializer.

---
+++ Src/Albedo/TypeElementMaterializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public class TypeElementMaterializer<T> : IReflectionElementMaterializer<T>
+    {
+        public IEnumerable<IReflectionElement> Materialize(IEnumerable<T> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
