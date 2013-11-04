116e8dfec0569b820b7fcb691971bdb0b91ede82
Mark Seemann <mark@ploeh.dk>
2013-11-04 23:20:00 +0100

    Documented FieldInfoElementMaterializer<T>.

--- Src/Albedo/FieldInfoElementMaterializer.cs
+++ Src/Albedo/FieldInfoElementMaterializer.cs
@@ -6,8 +6,36 @@ using System.Text;
 
 namespace Ploeh.Albedo
 {
+    /// <summary>
+    /// Creates <see cref="FieldInfoElement" /> instances from a sequence of
+    /// source objects.
+    /// </summary>
+    /// <typeparam name="T">The type of source objects.</typeparam>
+    /// <seealso cref="Materialize(IEnumerable{T})" />
     public class FieldInfoElementMaterializer<T> : IReflectionElementMaterializer<T>
     {
+        /// <summary>
+        /// Creates <see cref="FieldInfoElement" /> instances from a sequence
+        /// of source objects.
+        /// </summary>
+        /// <param name="source">The source objects.</param>
+        /// <returns>
+        /// A sequence of <see cref="FieldInfoElement" /> instances created
+        /// from <paramref name="source" />.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="source" /> is null.
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// This method creates <see cref="FieldInfoElement" /> instances from
+        /// all matching elements in <paramref name="source" />. An element is
+        /// matching if it's a <see cref="FieldInfo" /> instance, in which case
+        /// a corresponding <strong>FieldInfoElement</strong> is created and
+        /// returned.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="IReflectionElementMaterializer{T}" />
         public IEnumerable<IReflectionElement> Materialize(IEnumerable<T> source)
         {
             return source
