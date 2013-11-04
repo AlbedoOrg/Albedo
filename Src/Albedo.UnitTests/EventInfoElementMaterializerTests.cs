c06ede06dbb1e22ba4831bcf924ba600bbff7c32
Mark Seemann <mark@ploeh.dk>
2013-11-04 08:36:58 +0100

    Implemented Materialize method.

--- Src/Albedo.UnitTests/EventInfoElementMaterializerTests.cs
+++ Src/Albedo.UnitTests/EventInfoElementMaterializerTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using Ploeh.Albedo;
+using Xunit.Extensions;
+using System.Reflection;
 
 namespace Ploeh.Albedo.UnitTests
 {
@@ -14,6 +16,52 @@ namespace Ploeh.Albedo.UnitTests
         {
             var sut = new EventInfoElementMaterializer<object>();
             Assert.IsAssignableFrom<IReflectionElementMaterializer<object>>(sut);
+        }
+
+        [Theory, ClassData(typeof(SourceObjects))]
+        public void MaterializeObjectsReturnsCorrectResult(object[] objects)
+        {
+            var sut = new EventInfoElementMaterializer<object>();
+
+            var actual = sut.Materialize(objects);
+
+            var expected = objects
+                .OfType<EventInfo>()
+                .Select(ei => new EventInfoElement(ei))
+                .Cast<IReflectionElement>();
+            Assert.Equal(expected, actual);
+        }
+
+        private class SourceObjects : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new[]
+                {
+                    new object[]
+                    {
+                        typeof(AppDomain).GetEvents().First()
+                    }
+                };
+                yield return new[]
+                {
+                    typeof(AppDomain).GetEvents().Take(2).ToArray()
+                };
+                yield return new[]
+                {
+                    new object[]
+                    {
+                        typeof(AppDomain).GetEvents().First(),
+                        "",
+                        typeof(AppDomain).GetEvents().Skip(1).First(),
+                    }
+                };
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
         }
     }
 }
