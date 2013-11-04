a7fd92a5f45d4c6823541d4720989dc5244c8c7e
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-11-04 12:09:39 +0200

    Added XML documentation.

--- Src/Albedo/MethodInfoElementMaterializer.cs
+++ Src/Albedo/MethodInfoElementMaterializer.cs
@@ -6,8 +6,36 @@ using System.Text;
 
 namespace Ploeh.Albedo
 {
+    /// <summary>
+    /// Creates <see cref="MethodInfoElement" /> instances from a sequence of
+    /// source objects.
+    /// </summary>
+    /// <typeparam name="T">The type of source objects.</typeparam>
+    /// <seealso cref="Materialize(IEnumerable{T})" />
     public class MethodInfoElementMaterializer<T> : IReflectionElementMaterializer<T>
     {
+        /// <summary>
+        /// Creates <see cref="MethodInfoElement" /> instances from a sequence
+        /// of source objects.
+        /// </summary>
+        /// <param name="source">The source objects.</param>
+        /// <returns>
+        /// A sequence of <see cref="MethodInfoElement" /> instances created
+        /// from <paramref name="source" />.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="source" /> is null.
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// This method creates <see cref="MethodInfoElement" /> instances from
+        /// all matching elements in <paramref name="source" />. An element is
+        /// matching if it's an <see cref="MethodInfo" /> instance, in which
+        /// case a corresponding <strong>MethodInfoElement</strong> is created
+        /// and returned.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="IReflectionElementMaterializer{T}" />
         public IEnumerable<IReflectionElement> Materialize(IEnumerable<T> source)
         {
             return source
