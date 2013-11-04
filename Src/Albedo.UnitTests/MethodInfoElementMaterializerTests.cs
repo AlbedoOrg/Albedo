a8ea26d1bb6c37be94aacd840649d8b8bcaed4c7
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-11-04 11:53:56 +0200

    Introduced MethodInfoElementMaterializer<T> class.

---
+++ Src/Albedo.UnitTests/MethodInfoElementMaterializerTests.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Albedo;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class MethodInfoElementMaterializerTests
+    {
+        [Fact]
+        public void SutIsReflectionElementMaterializer()
+        {
+            var sut = new MethodInfoElementMaterializer<object>();
+            Assert.IsAssignableFrom<IReflectionElementMaterializer<object>>(sut);
+        }
+    }
+}
