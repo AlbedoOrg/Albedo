976329670e3ffed9ba83f9fc6eae23e56d077777
Mark Seemann <mark@ploeh.dk>
2013-11-04 08:41:51 +0100

    Documented EventInfoElementMaterializer<T>.

--- Src/Albedo/EventInfoElementMaterializer.cs
+++ Src/Albedo/EventInfoElementMaterializer.cs
@@ -6,8 +6,36 @@ using System.Text;
 
 namespace Ploeh.Albedo
 {
+    /// <summary>
+    /// Creates <see cref="EventInfoElement" /> instances from a sequence of
+    /// source objects.
+    /// </summary>
+    /// <typeparam name="T">The type of source objects.</typeparam>
+    /// <seealso cref="Materialize(IEnumerable{T})" />
     public class EventInfoElementMaterializer<T> : IReflectionElementMaterializer<T>
     {
+        /// <summary>
+        /// Creates <see cref="EventInfoElement" /> instances from a sequence
+        /// of source objects.
+        /// </summary>
+        /// <param name="source">The source objects.</param>
+        /// <returns>
+        /// A sequence of <see cref="EventInfoElement" /> instances created
+        /// from <paramref name="source" />.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="source" /> is null.
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// This method creates <see cref="EventInfoElement" /> instances from
+        /// all matching elements in <paramref name="source" />. An element is
+        /// matching if it's an <see cref="EventInfo" /> instance, in which
+        /// case a corresponding <strong>EventInfoElement</strong> is created
+        /// and returned.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="IReflectionElementMaterializer{T}" />
         public IEnumerable<IReflectionElement> Materialize(IEnumerable<T> source)
         {
             return source
