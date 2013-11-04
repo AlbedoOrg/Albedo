70d8dd029fcff5ca1302c8d244692c39e28bee60
Mark Seemann <mark@ploeh.dk>
2013-11-04 08:26:23 +0100

    Introduced EventInfoElementMaterializer.

---
+++ Src/Albedo.UnitTests/EventInfoElementMaterializerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Albedo;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class EventInfoElementMaterializerTests
+    {
+        [Fact]
+        public void SutIsReflectionElementMaterializer()
+        {
+            var sut = new EventInfoElementMaterializer<object>();
+            Assert.IsAssignableFrom<IReflectionElementMaterializer<object>>(sut);
+        }
+    }
+}
