220b82f8a1697b405cafc0a681571bd19ec5f79e
Mark Seemann <mark@ploeh.dk>
2013-11-07 15:46:14 +0100

    Refactored AssemblyElementMaterializerTests
    to look like the other Materializer tests.

--- Src/Albedo.UnitTests/AssemblyElementMaterializerTests.cs
+++ Src/Albedo.UnitTests/AssemblyElementMaterializerTests.cs
@@ -6,6 +6,7 @@ using Xunit;
 using Ploeh.Albedo;
 using Xunit.Extensions;
 using Moq;
+using System.Reflection;
 
 namespace Ploeh.Albedo.UnitTests
 {
@@ -18,20 +19,16 @@ namespace Ploeh.Albedo.UnitTests
             Assert.IsAssignableFrom<IReflectionElementMaterializer<object>>(sut);
         }
 
-        [Theory]
-        [InlineData(new object[] { new[] { typeof(Version) } })]
-        [InlineData(new object[] { new[] { typeof(AssemblyElement) } })]
-        [InlineData(new object[] { new[] { typeof(AssemblyElementTest) } })]
-        [InlineData(new object[] { new[] { typeof(AssemblyElement), typeof(Version) } })]
+        [Theory, ClassData(typeof(SourceObjects))]
         public void MaterializeAssembliesReturnsCorrectResult(
-            Type[] containedTypes)
+            object[] objects)
         {
-            var assemblies = containedTypes.Select(t => t.Assembly).ToArray();
             var sut = new AssemblyElementMaterializer<object>();
 
-            var actual = sut.Materialize(assemblies);
+            var actual = sut.Materialize(objects);
 
-            var expected = assemblies
+            var expected = objects
+                .OfType<Assembly>()
                 .Select(a => new AssemblyElement(a))
                 .Cast<IReflectionElement>();
             Assert.Equal(expected, actual);
@@ -42,6 +39,47 @@ namespace Ploeh.Albedo.UnitTests
         {
             var sut = new AssemblyElementMaterializer<object>();
             Assert.Throws<ArgumentNullException>(() => sut.Materialize(null));
+        }
+
+        private class SourceObjects : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new[]
+                {
+                    new object[]
+                    {
+                        typeof(Version).Assembly
+                    } 
+                };
+                yield return new[]
+                {
+                    new object[]
+                    {
+                        typeof(AssemblyElement).Assembly
+                    } 
+                };
+                yield return new[]
+                {
+                    new object[]
+                    {
+                        typeof(AssemblyElementTest).Assembly
+                    } 
+                };
+                yield return new[]
+                {
+                    new object[]
+                    {
+                        typeof(AssemblyElementTest).Assembly,
+                        typeof(Version).Assembly
+                    } 
+                };
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
         }
     }
 }
