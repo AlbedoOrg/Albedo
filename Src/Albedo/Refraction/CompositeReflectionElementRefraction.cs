0d51c4cff9dcd3c07e200e5a0f8536bf69c9a385
Mark Seemann <mark@ploeh.dk>
2013-11-09 23:05:01 +0100

    Documented CompositeReflectionElementRefraction<T>

--- Src/Albedo/Refraction/CompositeReflectionElementRefraction.cs
+++ Src/Albedo/Refraction/CompositeReflectionElementRefraction.cs
@@ -5,11 +5,38 @@ using System.Text;
 
 namespace Ploeh.Albedo.Refraction
 {
+    /// <summary>
+    /// Composes multiple <see cref="IReflectionElementRefraction{T}" />
+    /// instances, so that they look like a single instance of
+    /// <strong>IReflectionElementRefraction&lt;T&gt;</strong>.
+    /// </summary>
+    /// <typeparam name="T">The type of source objects.</typeparam>
+    /// <remarks>
+    /// <para>
+    /// This is a standard implementation of the Composite design pattern.
+    /// </para>
+    /// </remarks>
     public class CompositeReflectionElementRefraction<T> : 
-        IReflectionElementRefraction<T>, IEnumerable<IReflectionElementRefraction<T>>
+        IReflectionElementRefraction<T>,
+        IEnumerable<IReflectionElementRefraction<T>>
     {
         private readonly IEnumerable<IReflectionElementRefraction<T>> refractions;
 
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="CompositeReflectionElementRefraction{T}"/> class.
+        /// </summary>
+        /// <param name="refractions">The refractions to compose.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="refractions" /> is null.
+        /// </exception>
+        /// <remarks>
+        /// <para>
+        /// After construction, the injected <paramref name="refractions" />
+        /// are available by enumerating over the new instance.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="GetEnumerator" />
         public CompositeReflectionElementRefraction(
             params IReflectionElementRefraction<T>[] refractions)
         {
@@ -19,6 +46,20 @@ namespace Ploeh.Albedo.Refraction
             this.refractions = refractions;
         }
 
+        /// <summary>
+        /// Creates a sequence of <see cref="IReflectionElement" /> instances
+        /// from a sequence of source objects, but composing the result of
+        /// invoking <strong>Refract</strong> on all composed refractions.
+        /// </summary>
+        /// <param name="source">The source object.</param>
+        /// <returns>
+        /// A a sequence of <see cref="IReflectionElement" /> instances from a
+        /// sequence of source objects, but composing the result of invoking 
+        /// <strong>Refract</strong> on all composed refractions.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="source" /> is null.
+        /// </exception>
         public IEnumerable<IReflectionElement> Refract(IEnumerable<T> source)
         {
             if (source == null)
@@ -29,6 +70,21 @@ namespace Ploeh.Albedo.Refraction
                    select re;
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the composed
+        /// refractions.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that
+        /// can be used to iterate through the composed refractions.
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// The elements returned by this method are the refractions passed to
+        /// the constructor when the instance was created.
+        /// </para>
+        /// </remarks>
+        /// <seealso cref="CompositeReflectionElementRefraction(IReflectionElementRefraction{T}[])" />
         public IEnumerator<IReflectionElementRefraction<T>> GetEnumerator()
         {
             return this.refractions.GetEnumerator();
