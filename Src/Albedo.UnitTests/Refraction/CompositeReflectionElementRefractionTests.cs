d1bbf7889a423aa8b515b5300f008d6085ae196d
Mark Seemann <mark@ploeh.dk>
2013-11-09 20:50:24 +0100

    Introduced CompositeReflectionElementRefraction<T>,
    which will (obviously) fulfill the role of a Composite, so that the other,
    leaf IReflectionElementRefractions can be composed.

---
+++ Src/Albedo.UnitTests/Refraction/CompositeReflectionElementRefractionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Ploeh.Albedo;
+
+namespace Ploeh.Albedo.Refraction.UnitTests
+{
+    public class CompositeReflectionElementRefractionTests
+    {
+        [Fact]
+        public void SutIsReflectionElementRefraction()
+        {
+            var sut = new CompositeReflectionElementRefraction<object>();
+            Assert.IsAssignableFrom<IReflectionElementRefraction<object>>(sut);
+        }
+    }
+}
---
+++ Src/Albedo/Refraction/CompositeReflectionElementRefraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Albedo.Refraction
+{
+    public class CompositeReflectionElementRefraction<T> : 
+        IReflectionElementRefraction<T>
+    {
+        public IEnumerable<IReflectionElement> Refract(IEnumerable<T> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
