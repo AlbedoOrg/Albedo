9480f0b230bd41320e6aeb6f41b561f974b86e1e
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2013-11-10 21:04:17 +0200

    Introduced ReflectionVisitor<T> class.

---
+++ Src/Albedo.UnitTests/ReflectionVisitorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class ReflectionVisitorTests
+    {
+        [Fact]
+        public void SutIsReflectionVisitor()
+        {
+            var sut = new ReflectionVisitor<int>();
+            Assert.IsAssignableFrom<IReflectionVisitor<int>>(sut);
+        }
+    }
+}
---
+++ Src/Albedo/ReflectionVisitor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public class ReflectionVisitor<T> : IReflectionVisitor<T>
+    {
+        public T Value
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IReflectionVisitor<T> Visit(AssemblyElement assemblyElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(ConstructorInfoElement constructorInfoElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(FieldInfoElement fieldInfoElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(MethodInfoElement methodInfoElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(ParameterInfoElement parameterInfoElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(PropertyInfoElement propertyInfoElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(TypeElement typeElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(LocalVariableInfoElement localVariableInfoElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReflectionVisitor<T> Visit(EventInfoElement eventInfoElement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
