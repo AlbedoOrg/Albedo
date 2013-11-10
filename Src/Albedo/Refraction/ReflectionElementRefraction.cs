35889c1d59c5a6f729835a4248230ee921721ae6
Mark Seemann <mark@ploeh.dk>
2013-11-10 09:38:50 +0100

    Documented ReflectionElementRefraction<T>.

--- Src/Albedo/Refraction/ReflectionElementRefraction.cs
+++ Src/Albedo/Refraction/ReflectionElementRefraction.cs
@@ -5,8 +5,25 @@ using System.Text;
 
 namespace Ploeh.Albedo.Refraction
 {
+    /// <summary>
+    /// Filters a sequence of source objects, allowing only
+    /// <see cref="IReflectionElement" /> instances to pass through.
+    /// </summary>
+    /// <typeparam name="T">The type of source objects.</typeparam>
     public class ReflectionElementRefraction<T> : IReflectionElementRefraction<T>
     {
+        /// <summary>
+        /// Filters a sequence of source objects, allowing only
+        /// <see cref="IReflectionElement" /> instances to pass through.
+        /// </summary>
+        /// <param name="source">The source objects.</param>
+        /// <returns>
+        /// A sequence of only those elements of <paramref name="source" />
+        /// that are already <see cref="IReflectionElement" /> instances.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="source" /> is null.
+        /// </exception>
         public IEnumerable<IReflectionElement> Refract(IEnumerable<T> source)
         {
             return source.OfType<IReflectionElement>();
