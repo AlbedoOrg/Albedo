abfc87095ed275146a5c44bdd1c9b082cc7eac2a
Mark Seemann <mark@ploeh.dk>
2013-11-10 10:56:23 +0100

    Documented AsReflectionElement method.

--- Src/Albedo/Refraction/Conversions.cs
+++ Src/Albedo/Refraction/Conversions.cs
@@ -5,8 +5,30 @@ using System.Text;
 
 namespace Ploeh.Albedo.Refraction
 {
+    /// <summary>
+    /// Contains conversion methods to turn objects into
+    /// <see cref="IReflectionElement" /> instances.
+    /// </summary>
     public static class Conversions
     {
+        /// <summary>
+        /// Converts an object to an <see cref="IReflectionElement" />
+        /// instance. This method is guaranteed to always return a proper
+        /// instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to convert.</typeparam>
+        /// <param name="source">The object to convert.</param>
+        /// <returns>
+        /// An <see cref="IReflectionElement" /> instance that adapts
+        /// <paramref name="source" />, if possibly; otherwise, a
+        /// <see cref="NullReflectionElement" />.
+        /// </returns>
+        /// <remarks>
+        /// This method is guaranteed to return an
+        /// <see cref="IReflectionElement" /> instance. However, if
+        /// <paramref name="source" /> can't properly be adapted, a
+        /// <see cref="NullReflectionElement" /> is returned.
+        /// </remarks>
         public static IReflectionElement AsReflectionElement<T>(this T source)
         {
             return new CompositeReflectionElementRefraction<T>(
