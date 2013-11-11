3b092af3df090856ba9fcc3485c25ef535763805
Mark Seemann <mark@ploeh.dk>
2013-11-11 21:51:31 +0100

    Added ability to sign the release build
    from the bash script.

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe `dirname $0`/BuildRelease.msbuild
 
+function abspath()
+{
+	# Make an absolute path from a relative path, and convert it from Linux to Windows format
+	# Lifted from
+	# http://stackoverflow.com/a/3915420/126014
+	# http://stackoverflow.com/a/13701495/126014
+	echo $(cd $(dirname $1); pwd)/$(basename $1) | sed 's/^\///' | sed 's/\//\\/g' | sed 's/^./\0:/'
+}
+
+if [ "$1" != "" ]; then
+    $WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe /property:AssemblyOriginatorKeyFile=`abspath $1` `dirname $0`/BuildRelease.msbuild
+else
+    $WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe `dirname $0`/BuildRelease.msbuild
+fi
