f8668aed716cf51d7f8e1ef6f172c9ce40875cb4
Adam Chester <adamchester@gmail.com>
2013-12-27 16:43:51 +1000

    Documented ReflectionElementExtensions.Accept()

--- Src/Albedo/ReflectionElementExtensions.cs
+++ Src/Albedo/ReflectionElementExtensions.cs
@@ -11,6 +11,23 @@ namespace Ploeh.Albedo
     /// </summary>
     public static class ReflectionElementExtensions
     {
+        /// <summary>
+        /// Accepts the <see cref="IReflectionVisitor{T}"/> visitor on each of the
+        /// <paramref name="elements"/> in the sequence.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of observation or result which the <see cref="IReflectionVisitor{T}"/>
+        /// instance produces when visiting nodes.
+        /// </typeparam>
+        /// <param name="elements">
+        /// The sequence of <see cref="IReflectionElement"/> instances upon which 
+        /// the <see cref="IReflectionElement.Accept{T}"/> method will be called.
+        /// </param>
+        /// <param name="visitor">The <see cref="IReflectionVisitor{T}"/> instance.</param>
+        /// <returns>
+        /// A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.
+        /// </returns>
         public static IReflectionVisitor<T> Accept<T>(
             this IEnumerable<IReflectionElement> elements,
             IReflectionVisitor<T> visitor)
