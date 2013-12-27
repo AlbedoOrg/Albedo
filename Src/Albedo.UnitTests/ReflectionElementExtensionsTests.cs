9f5b5898042019c75d5749ed827354bdba7d1b8e
Adam Chester <adamchester@gmail.com>
2013-12-27 16:29:29 +1000

    Introduced ReflectionElementExtensions,
    to expose extension methods related to producing or consuming IReflectionElement instances.

---
+++ Src/Albedo.UnitTests/ReflectionElementExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class ReflectionElementExtensionsTests
+    {
+    }
+}
---
+++ Src/Albedo/ReflectionElementExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Ploeh.Albedo
+{
+    /// <summary>
+    /// Contains extension methods for dealing with <see cref="IReflectionElement"/>
+    /// instances, both producing and consuming them.
+    /// </summary>
+    public static class ReflectionElementExtensions
+    {
+    }
+}
