d82f3aeb07f4f6cb2d1e289dd3cef21a7ceada6e
Adam Chester <adamchester@gmail.com>
2013-12-30 01:12:29 +1000

    Added ReflectionElementEnvy.GetProperMethods(),
    providing a more consistent way to perform the common query: methods, excluding property accessors.

--- Src/Albedo/ReflectionElementEnvy.cs
+++ Src/Albedo/ReflectionElementEnvy.cs
@@ -12,6 +12,30 @@ namespace Ploeh.Albedo
     public static class ReflectionElementEnvy
     {
         /// <summary>
+        /// Gets 'proper' methods from the <paramref name="type"/>; that is, methods excluding
+        /// property accessors, and matching the provided <paramref name="bindingAttr"/>.
+        /// </summary>
+        /// <param name="type">The <see cref="Type"/> from which methods are selected</param>
+        /// <param name="bindingAttr">The <see cref="BindingFlags"/> used to determine which
+        /// methods to return.</param>
+        /// <returns>The sequence of <see cref="IReflectionElement"/> instances representing
+        /// the matching methods found on the <paramref name="type"/>.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly",
+           MessageId = "Attr", Justification = "This is the standard naming pattern for a BindingFlags parameter, used in the .NET Framework.")]
+        public static IEnumerable<IReflectionElement> GetProperMethods(
+            this Type type, BindingFlags bindingAttr)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+            return type
+                .GetMethods(bindingAttr)
+                .Except(type
+                    .GetProperties()
+                    .SelectMany(pi => pi.GetAccessors()))
+                .Select(mi => new MethodInfoElement(mi))
+                .Cast<IReflectionElement>();
+        }
+
+        /// <summary>
         /// Accepts the <see cref="IReflectionVisitor{T}"/> visitor on each of the
         /// <paramref name="elements"/> in the sequence.
         /// </summary>
