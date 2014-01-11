c37e4b77893b59501f58dfc1b0b37bd77e1f7e9a
Adam Chester <adamchester@gmail.com>
2014-01-11 11:56:19 +1000

    Implemented ToElement methods.

--- Src/Albedo.UnitTests/ConcreteElementConversionsTests.cs
+++ Src/Albedo.UnitTests/ConcreteElementConversionsTests.cs
@@ -66,10 +66,119 @@ namespace Ploeh.Albedo.UnitTests
         }
 
         [Fact]
-        public void ToElementForTypeThrowsOnNullType()
+        public void ToElementForAssemblyReturnsTheCorrectAssembly()
         {
-            Assert.Throws<ArgumentNullException>(() =>
-                ConcreteElementConversions.ToElement((Type) null));
+            Assembly expected = typeof (TypeWithAllElements).Assembly;
+            AssemblyElement actual = expected.ToElement();
+            Assert.Equal(new AssemblyElement(expected), actual);
         }
+
+        [Fact]
+        public void ToElementForConstructorInfoReturnsTheCorrectElement()
+        {
+            ConstructorInfo expected = typeof (TypeWithAllElements).GetConstructors().Single();
+            ConstructorInfoElement actual = expected.ToElement();
+            Assert.Equal(new ConstructorInfoElement(expected), actual);
+        }
+
+        [Fact]
+        public void ToElementForEventInfoReturnsTheCorrectElement()
+        {
+            EventInfo expected = typeof(TypeWithAllElements).GetEvents().Single();
+            EventInfoElement actual = expected.ToElement();
+            Assert.Equal(new EventInfoElement(expected), actual);
+        }
+
+        [Fact]
+        public void ToElementForFieldInfoReturnsTheCorrectElement()
+        {
+            FieldInfo expected = typeof(TypeWithAllElements).GetFields().Single();
+            FieldInfoElement actual = expected.ToElement();
+            Assert.Equal(new FieldInfoElement(expected), actual);
+        }
+
+        [Fact]
+        public void ToElementForLocalVariableInfoReturnsTheCorrectElement()
+        {
+            LocalVariableInfo expected = TypeWithAllElements.LocalVariableInfo;
+            LocalVariableInfoElement actual = expected.ToElement();
+            Assert.Equal(new LocalVariableInfoElement(expected), actual);
+        }
+
+        [Fact]
+        public void ToElementForMethodInfoReturnsTheCorrectElement()
+        {
+            MethodInfo expected = typeof (TypeWithAllElements).GetMethods()[0];
+            MethodInfoElement actual = expected.ToElement();
+            Assert.Equal(new MethodInfoElement(expected), actual);
+        }
+
+        [Fact]
+        public void ToElementForParameterInfoReturnsTheCorrectElement()
+        {
+            ParameterInfo expected = TypeWithAllElements.ParameterInfo;
+            ParameterInfoElement actual = expected.ToElement();
+            Assert.Equal(new ParameterInfoElement(expected), actual);
+        }
+
+        [Fact]
+        public void ToElementForPropertyInfoReturnsTheCorrectElement()
+        {
+            PropertyInfo expected = typeof (TypeWithAllElements).GetProperty("TheProperty");
+            PropertyInfoElement actual = expected.ToElement();
+            Assert.Equal(new PropertyInfoElement(expected), actual);
+        }
+
+        [Fact]
+        public void ToElementForTypeReturnsTheCorrectElement()
+        {
+            Type expected = typeof (TypeWithAllElements);
+            TypeElement actual = expected.ToElement();
+            Assert.Equal(new TypeElement(expected), actual);
+        }
+        
+        class TypeWithAllElements
+        {
+#pragma warning disable 67
+            public event EventHandler TheOnlyEvent;
+#pragma warning restore 67
+
+#pragma warning disable 169
+            public int TheOnlyField;
+#pragma warning restore 169
+
+            public static void MethodWithLocal(int parameter)
+            {
+                // This is required to prevent the compiler from
+                // warning and optimising away the local variable.
+                var local = 1;
+                local = local + 1;
+                local = local + 2;
+            }
+
+            public static LocalVariableInfo LocalVariableInfo
+            {
+                get
+                {
+                    return typeof (TypeWithAllElements)
+                        .GetMethod("MethodWithLocal")
+                        .GetMethodBody()
+                        .LocalVariables[0];
+                }
+            }
+
+            public static ParameterInfo ParameterInfo
+            {
+                get
+                {
+                    return typeof (TypeWithAllElements)
+                        .GetMethod("MethodWithLocal")
+                        .GetParameters()[0];
+                }
+            }
+
+            public static int TheProperty { get; set; }
+        }
+
     }
 }
--- Src/Albedo/ConcreteElementConversions.cs
+++ Src/Albedo/ConcreteElementConversions.cs
@@ -11,55 +11,55 @@ namespace Ploeh.Albedo
         public static AssemblyElement ToElement(this Assembly assembly)
         {
             if (assembly == null) throw new ArgumentNullException("assembly");
-            throw new NotImplementedException();
+            return new AssemblyElement(assembly);
         }
 
         public static ConstructorInfoElement ToElement(this ConstructorInfo constructorInfo)
         {
             if (constructorInfo == null) throw new ArgumentNullException("constructorInfo");
-            throw new NotImplementedException();
+            return new ConstructorInfoElement(constructorInfo);
         }
 
         public static EventInfoElement ToElement(this EventInfo eventInfo)
         {
             if (eventInfo == null) throw new ArgumentNullException("eventInfo");
-            throw new NotImplementedException();
+            return new EventInfoElement(eventInfo);
         }
 
         public static FieldInfoElement ToElement(this FieldInfo fieldInfo)
         {
             if (fieldInfo == null) throw new ArgumentNullException("fieldInfo");
-            throw new NotImplementedException();
+            return new FieldInfoElement(fieldInfo);
         }
 
         public static LocalVariableInfoElement ToElement(this LocalVariableInfo localVariableInfo)
         {
             if (localVariableInfo == null) throw new ArgumentNullException("localVariableInfo");
-            throw new NotImplementedException();
+            return new LocalVariableInfoElement(localVariableInfo);
         }
 
         public static MethodInfoElement ToElement(this MethodInfo methodInfo)
         {
             if (methodInfo == null) throw new ArgumentNullException("methodInfo");
-            throw new NotImplementedException();
+            return new MethodInfoElement(methodInfo);
         }
 
         public static ParameterInfoElement ToElement(this ParameterInfo parameterInfo)
         {
             if (parameterInfo == null) throw new ArgumentNullException("parameterInfo");
-            throw new NotImplementedException();
+            return new ParameterInfoElement(parameterInfo);
         }
 
-        public static PropertyInfoElement ToElement(PropertyInfo propertyInfo)
+        public static PropertyInfoElement ToElement(this PropertyInfo propertyInfo)
         {
             if (propertyInfo == null) throw new ArgumentNullException("propertyInfo");
-            throw new NotImplementedException();
+            return new PropertyInfoElement(propertyInfo);
         }
 
         public static TypeElement ToElement(this Type type)
         {
             if (type == null) throw new ArgumentNullException("type");
-            throw new NotImplementedException();
+            return new TypeElement(type);
         }
     }
 }
