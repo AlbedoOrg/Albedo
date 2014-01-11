68befdf4b64cac4ea64a60ee7031250b009b8c3b
Adam Chester <adamchester@gmail.com>
2014-01-11 11:30:59 +1000

    Introduced concrete element conversions,
    providing a clean syntax to convert from System.Reflection.* elements into Albedo *Element instances.

---
+++ Src/Albedo.UnitTests/ConcreteElementConversionsTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class ConcreteElementConversionsTests
+    {
+        [Fact]
+        public void ToElementForAssemblyThrowsOnNullAssembly()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((Assembly) null));
+        }
+
+        [Fact]
+        public void ToElementForConstructorInfoThrowsOnNullConstructorInfo()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((ConstructorInfo) null));
+        }
+
+        [Fact]
+        public void ToElementForEventInfoThrowsOnNullEventInfo()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((EventInfo) null));
+        }
+
+        [Fact]
+        public void ToElementForFieldInfoThrowsOnNullFieldInfo()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((FieldInfo) null));
+        }
+
+        [Fact]
+        public void ToElementForLocalVariableInfoThrowsOnNullLocalVariableInfo()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((LocalVariableInfo) null));
+        }
+
+        [Fact]
+        public void ToElementForMethodInfoThrowsOnNullMethodInfo()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((MethodInfo) null));
+        }
+
+        [Fact]
+        public void ToElementForParameterInfoThrowsOnNullParameterInfo()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((ParameterInfo) null));
+        }
+
+        [Fact]
+        public void ToElementForPropertyInfoThrowsOnNullPropertyInfo()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((PropertyInfo) null));
+        }
+
+        [Fact]
+        public void ToElementForTypeThrowsOnNullType()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                ConcreteElementConversions.ToElement((Type) null));
+        }
+    }
+}
---
+++ Src/Albedo/ConcreteElementConversions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Ploeh.Albedo
+{
+    public static class ConcreteElementConversions
+    {
+        public static AssemblyElement ToElement(this Assembly assembly)
+        {
+            if (assembly == null) throw new ArgumentNullException("assembly");
+            throw new NotImplementedException();
+        }
+
+        public static ConstructorInfoElement ToElement(this ConstructorInfo constructorInfo)
+        {
+            if (constructorInfo == null) throw new ArgumentNullException("constructorInfo");
+            throw new NotImplementedException();
+        }
+
+        public static EventInfoElement ToElement(this EventInfo eventInfo)
+        {
+            if (eventInfo == null) throw new ArgumentNullException("eventInfo");
+            throw new NotImplementedException();
+        }
+
+        public static FieldInfoElement ToElement(this FieldInfo fieldInfo)
+        {
+            if (fieldInfo == null) throw new ArgumentNullException("fieldInfo");
+            throw new NotImplementedException();
+        }
+
+        public static LocalVariableInfoElement ToElement(this LocalVariableInfo localVariableInfo)
+        {
+            if (localVariableInfo == null) throw new ArgumentNullException("localVariableInfo");
+            throw new NotImplementedException();
+        }
+
+        public static MethodInfoElement ToElement(this MethodInfo methodInfo)
+        {
+            if (methodInfo == null) throw new ArgumentNullException("methodInfo");
+            throw new NotImplementedException();
+        }
+
+        public static ParameterInfoElement ToElement(this ParameterInfo parameterInfo)
+        {
+            if (parameterInfo == null) throw new ArgumentNullException("parameterInfo");
+            throw new NotImplementedException();
+        }
+
+        public static PropertyInfoElement ToElement(PropertyInfo propertyInfo)
+        {
+            if (propertyInfo == null) throw new ArgumentNullException("propertyInfo");
+            throw new NotImplementedException();
+        }
+
+        public static TypeElement ToElement(this Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+            throw new NotImplementedException();
+        }
+    }
+}
