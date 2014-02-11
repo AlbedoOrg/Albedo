ba6a743167afa2eb67052e844b48a36062225860
Jin-Wook Chung <jwChung@hotmail.com>
2014-02-12 02:22:37 +0900

    Moved TypeWithLocalVariable to outer scope.

--- Src/Albedo.UnitTests/LocalVariableInfoElementTests.cs
+++ Src/Albedo.UnitTests/LocalVariableInfoElementTests.cs
@@ -124,48 +124,5 @@ namespace Ploeh.Albedo.UnitTests
             var expected = lvi.GetHashCode();
             Assert.Equal(expected, actual);
         }
-
-
-        class TypeWithLocalVariable
-        {
-            public static LocalVariableInfo LocalVariable
-            {
-                get
-                {
-                    return typeof(TypeWithLocalVariable)
-                        .GetMethod("TheMethod")
-                        .GetMethodBody()
-                        .LocalVariables[0];
-                }
-            }
-            public static LocalVariableInfo OtherLocalVariable
-            {
-                get
-                {
-                    return typeof(TypeWithLocalVariable)
-                        .GetMethod("TheOtherMethod")
-                        .GetMethodBody()
-                        .LocalVariables[0];
-                }
-            }
-
-            public void TheMethod()
-            {
-                // This is required to prevent the compiler from
-                // warning and optimising away the local variable.
-                var local = 1;
-                local = local + 1;
-                local = local + 2;
-            }
-
-            public void TheOtherMethod()
-            {
-                // This is required to prevent the compiler from
-                // warning and optimising away the local variable.
-                var local = 1;
-                local = local + 1;
-                local = local + 2;
-            }
-        }
     }
 }
