ab4c9ecf2daf2a2abee95ae5ea1c71ab9de389e6
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-05 00:06:38 +0900

    Refactored the two Select methods.

--- Src/Albedo/Constructors.cs
+++ Src/Albedo/Constructors.cs
@@ -8,24 +8,15 @@ namespace Ploeh.Albedo
     {
         public ConstructorInfo Select<T>(Expression<Func<T>> constructorSelector)
         {
-            if (constructorSelector == null)
-            {
-                throw new ArgumentNullException("constructorSelector");
-            }
-            
-            var newExpression = constructorSelector.Body as NewExpression;
-            if (newExpression == null)
-            {
-                throw new ArgumentException(
-                    "The expression's body must be a NewExpression. " +
-                    "The code block supplied should construct an new instance.\nExample: () => new Foo().",
-                    "constructorSelector");
-            }
-
-            return newExpression.Constructor;
+            return SelectImpl(constructorSelector);
         }
 
         public ConstructorInfo Select<T>(Expression<Func<object, T>> constructorSelector)
+        {
+            return SelectImpl(constructorSelector);
+        }
+
+        private static ConstructorInfo SelectImpl<TFunc>(Expression<TFunc> constructorSelector)
         {
             if (constructorSelector == null)
             {
