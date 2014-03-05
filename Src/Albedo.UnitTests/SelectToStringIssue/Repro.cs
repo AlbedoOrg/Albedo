695899b4dbf3e29d9c90e55fe19866c21cf363ac
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-03-05 07:58:19 +0200

    Added the original 'repro' test case as reported by @jwChung in #64.

---
+++ Src/Albedo.UnitTests/SelectToStringIssue/Repro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.Albedo.UnitTests.SelectToStringIssue
+{
+    public class Repro
+    {
+        [Fact]
+        public void SelectMethodDeclaredOnBaseReturnsCorrectMethod()
+        {
+            var sut = new Methods<ClassWithMethods>();
+            var expected = typeof(ClassWithMethods).GetMethod("ToString");
+
+            var actual = sut.Select(x => x.ToString());
+
+            Assert.Equal(expected, actual);
+        }
+
+        private class ClassWithMethods
+        {
+        }
+    }
+}
