5037f6d955f05a4c22b9733857105d1b75f9e834
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-11 15:44:07 +0900

    Remove unnecessary using directives.

--- Src/Albedo.UnitTests/MethodsTests.cs
+++ Src/Albedo.UnitTests/MethodsTests.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using Xunit;
-using Ploeh.Albedo;
 using System.Reflection;
 
 namespace Ploeh.Albedo.UnitTests
