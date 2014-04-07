96376f25225a24ae74fda9c22926b54d413cbaa6
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-07 11:47:39 +0900

    Clarified the exception message of Properties.Select.

--- Src/Albedo/Properties.cs
+++ Src/Albedo/Properties.cs
@@ -151,7 +151,7 @@ namespace Ploeh.Albedo
         /// The expression's body isn't a MemberExpression or it doesn't
         /// identify a property. The code block supplied should identify a
         /// property.
-        /// Example: () => x.Bar.
+        /// Example: () => Foo.Bar.
         /// </exception>
         /// <seealso cref="Properties{T}" />
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1011:ConsiderPassingBaseTypesAsParameters", Justification = "The expression is strongly typed in order to prevent the caller from passing any sort of expression. It doesn't fully capture everything the caller might throw at it, but it does constrain the caller as well as possible. This enables the developer to get a compile-time exception instead of a run-time exception in most cases where an invalid expression is being supplied.")]
@@ -162,7 +162,7 @@ namespace Ploeh.Albedo
 
             var memberExp = propertySelector.Body as MemberExpression;
             if (memberExp == null)
-                throw new ArgumentException("The expression's body must be a MemberExpression. The code block supplied should identify a property.\nExample: x => x.Bar.", "propertySelector");
+                throw new ArgumentException("The expression's body must be a MemberExpression. The code block supplied should identify a property.\nExample: () => Foo.Bar.", "propertySelector");
 
             var pi = memberExp.Member as PropertyInfo;
             if (pi == null)
