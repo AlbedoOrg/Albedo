31f871331748b0a5a16e41126a36ddc7588372be
Alex Povar <zvirja1@gmail.com>
2017-08-28 14:00:09 +0300

    Enable assembly attributes generation for static parameters

--- Src/Albedo.UnitTests/Properties/AssemblyInfo.cs
+++ Src/Albedo.UnitTests/Properties/AssemblyInfo.cs
@@ -1,16 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Albedo.UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Albedo.UnitTests")]
-[assembly: AssemblyCopyright("Copyright © Mark Seemann 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Src/Albedo/Properties/AssemblyInfo.cs
+++ Src/Albedo/Properties/AssemblyInfo.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Albedo")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Albedo")]
-[assembly: AssemblyCopyright("Copyright © Mark Seemann 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
