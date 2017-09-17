a2b0d75d8579aab5e16cc3bfa464fef25d902c00
Alex Povar <zvirja1@gmail.com>
2017-09-17 23:39:52 +0300

    Download nuget binary during build for FAKE installation
    On some environments NuGet might be not available in the PATH
    variable. To cover such environments we use our own nuget binary.

--- build.cmd
+++ build.cmd
@@ -1,8 +1,12 @@
 @echo off
 cls
+IF NOT EXIST "build\tools\nuget.exe" (
+  mkdir "build\tools\"
+  powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v4.3.0/nuget.exe -OutFile build\tools\nuget.exe"
+)
 
 IF NOT EXIST "build\tools\FAKE.Core\tools\Fake.exe" (
-  nuget install "FAKE.Core" -OutputDirectory "build\tools" -ExcludeVersion -Version 4.63.2
+  "build\tools\nuget.exe" install "FAKE.Core" -OutputDirectory "build\tools" -ExcludeVersion -Version 4.63.2
 )
 
 "build\tools\FAKE.Core\tools\Fake.exe" build.fsx %*
